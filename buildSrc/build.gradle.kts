71fc79934e8fec88ac70d4cd787ad95912c6a2ff
Vahid Moradi <zvm4zvm@gmail.com>
2025-02-09 17:50:12 +0100

    #7 Add Android library module plugin and set the local data module's namespace.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,6 +7,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
+    //noinspection UseTomlInstead
     implementation("com.android.tools.build:gradle:8.8.0")
     implementation(gradleApi())
     implementation(kotlin("gradle-plugin", version = "2.0.0"))
