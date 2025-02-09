eafd94af4d97169f5389de279fc4197e823f6354
Vahid Moradi <zvm4zvm@gmail.com>
2025-02-09 17:55:54 +0100

    #7 Added main domain module

---
+++ features/main/domain/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id(libs.plugins.android.library.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+}
+
+android {
+    namespace = "com.epiclabs.thinktok.main.domain"
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
