7069126cf0478b700e8907a90bae2834ab89cd48
Vahid Moradi <zvm4zvm@gmail.com>
2025-02-13 22:47:44 +0100

    #25 Refactored Android modules configuration
    - Moved common Android configuration code to `common.kt`.
    - Applied common configurations to both app and library modules.
    - Configured Kotlin JVM target to 21 for all projects.
    - Enabled JUnit platform for all test tasks.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,11 @@
 plugins {
-    id(libs.plugins.android.application.get().pluginId)
-    id(libs.plugins.kotlin.android.get().pluginId) 
+    alias(libs.plugins.app.module)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.ksp) apply false
-    alias(libs.plugins.app.module)
 }
 
 android {
     namespace = "com.epiclabs.thinktok"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.epiclabs.thinktok"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
-    }
-    kotlinOptions {
-        jvmTarget = "21"
-    }
-    buildFeatures {
-        compose = true
-    }
-}
-
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform()
 }
 
 dependencies {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.kotlin.compose) apply false
@@ -5,4 +8,18 @@ plugins {
     id(libs.plugins.android.application.get().pluginId) apply false
     id(libs.plugins.android.library.get().pluginId) apply false
     id(libs.plugins.kotlin.android.get().pluginId) apply false
+}
+
+allprojects {
+    // Apply to all projects (root and subprojects)
+    tasks.withType<KotlinCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+    }
+
 }
