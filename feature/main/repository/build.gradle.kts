4105cd0e23ad8868f525c2d135b06092ed44814b
M.Mahdi Behroozfar <mmbehroozfar@gmail.com>
2025-02-14 22:02:03 +0330

    #10 Add koin dependency to all modules

--- feature/main/repository/build.gradle.kts
+++ feature/main/repository/build.gradle.kts
@@ -10,6 +10,12 @@ dependencies {
     implementation(project(":feature:main:repository:api"))
     implementation(project(":feature:main:domain:api"))
     implementation(libs.androidx.core.ktx)
+
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    testImplementation(libs.koin.test)
+    testImplementation(libs.koin.junit4)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
