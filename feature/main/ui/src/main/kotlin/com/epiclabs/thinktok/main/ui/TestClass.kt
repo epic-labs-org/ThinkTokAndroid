424164403c4e43d18ae6bc1564532df51a8ccebb
Vahid Moradi <zvm4zvm@gmail.com>
2025-02-16 22:16:20 +0100

    Removed `TestClass` and `TestClass2` from the main UI module.

--- feature/main/ui/src/main/kotlin/com/epiclabs/thinktok/main/ui/TestClass.kt
+++
@@ -1,6 +0,0 @@
-package com.epiclabs.thinktok.main.ui
-
-class TestClass {
-    private val
-    testProperty: String = "Test Prop   erty"
-}
--- feature/main/ui/src/main/kotlin/com/epiclabs/thinktok/main/ui/TestClass2.kt
+++
@@ -1,6 +0,0 @@
-package com.epiclabs.thinktok.main.ui
-
-class TestClass2 {
-    private val
-    testProperty: String = "Test Prop   erty"
-}
