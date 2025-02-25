23bc7fde3fd9b407eabf0936273af1e9bf811c34
mbn <noorianmb@gmail.com>
2025-02-25 22:12:21 +0330

    Add SplashConstant to keep Splash duration

--- app/src/main/kotlin/com/epiclabs/thinktok/MainActivity.kt
+++ app/src/main/kotlin/com/epiclabs/thinktok/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.lifecycleScope
+import com.epiclabs.thinktok.constants.SplashConstant
 import com.epiclabs.thinktok.ui.theme.ThinkTokTheme
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -21,12 +22,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashscreen = installSplashScreen()
         var keepSplashScreen = true
+
         super.onCreate(savedInstanceState)
+
         splashscreen.setKeepOnScreenCondition { keepSplashScreen }
+
         lifecycleScope.launch {
-            delay(200)
+            delay(SplashConstant.SPLASH_DURATION_MS)
             keepSplashScreen = false
         }
+
         enableEdgeToEdge()
         setContent {
             ThinkTokTheme {
