2bc5f3313a708800e05db907f2cb51228387d220
Vahid Moradi <zvm4zvm@gmail.com>
2025-03-02 14:17:33 +0100

    Refactor: Added test tags to LanguageDropdown component
    - Added test tags to the LanguageDropdown composable, improving UI testability.
    - Introduced constants for test tag strings.
    - Added a test tag to the LanguageDropdownTextField
    - Added a test tag to the LanguageDropdownMenu
    - Added a test tag to each LanguageDropdownMenuItem, with a dynamic suffix for the language.

--- feature/main/ui/src/main/kotlin/com/epiclabs/thinktok/main/ui/languageselection/LanguageDropdown.kt
+++ feature/main/ui/src/main/kotlin/com/epiclabs/thinktok/main/ui/languageselection/LanguageDropdown.kt
@@ -16,6 +16,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 
+private const val TEST_TAG_LANGUAGE_DROPDOWN_TEXT_FIELD = "LanguageDropdownTextField"
+private const val TEST_TAG_LANGUAGE_DROPDOWN_MENU = "LanguageDropdownMenu"
+private const val TEST_TAG_LANGUAGE_DROPDOWN_MENU_ITEM = "LanguageDropdownMenuItem-%s"
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun LanguageDropdown(
@@ -44,19 +48,19 @@ internal fun LanguageDropdown(
             trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
             modifier =
                 Modifier
-                    .testTag("LanguageDropdownTextField")
+                    .testTag(TEST_TAG_LANGUAGE_DROPDOWN_TEXT_FIELD)
                     .menuAnchor()
                     .fillMaxWidth(),
         )
 
         ExposedDropdownMenu(
-            modifier = Modifier.fillMaxWidth().testTag("LanguageDropdownMenu"),
+            modifier = Modifier.fillMaxWidth().testTag(TEST_TAG_LANGUAGE_DROPDOWN_MENU),
             expanded = expanded,
             onDismissRequest = { expanded = false },
         ) {
             languages.forEach { language ->
                 DropdownMenuItem(
-                    modifier = Modifier.fillMaxWidth().testTag("LanguageDropdownMenuItem-$language"),
+                    modifier = Modifier.fillMaxWidth().testTag(TEST_TAG_LANGUAGE_DROPDOWN_MENU_ITEM.format(language)),
                     text = { Text(text = language) },
                     onClick = {
                         selectedLanguage = language
