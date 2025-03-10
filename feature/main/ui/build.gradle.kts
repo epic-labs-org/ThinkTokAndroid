6e66a46d1b71958682ee250a1c835d017d9252de
Vahid Moradi <zvm4zvm@gmail.com>
2025-03-10 23:03:11 +0100

    Removed Compose ConstraintLayout dependency.
    - Removed `androidx.constraintlayout.compose` dependency from `libs.versions.toml`.
    - Removed `implementation(libs.androidx.constraintlayout.compose)` from `feature/main/ui/build.gradle.kts`.

--- feature/main/ui/build.gradle.kts
+++ feature/main/ui/build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.constraintlayout.compose)
 
     debugImplementation(libs.androidx.ui.tooling)
     implementation(libs.androidx.ui.tooling.preview)
