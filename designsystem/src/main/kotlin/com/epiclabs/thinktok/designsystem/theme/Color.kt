778ab32a7617db829fcb814a6cb0feb4b5d8b7de
mbn <noorianmb@gmail.com>
2025-04-26 00:25:44 +0330

    use material3 style for define color

--- designsystem/src/main/kotlin/com/epiclabs/thinktok/designsystem/theme/Color.kt
+++ designsystem/src/main/kotlin/com/epiclabs/thinktok/designsystem/theme/Color.kt
@@ -2,17 +2,78 @@ package com.epiclabs.thinktok.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Primary = Color(0xFF0078D9)
-val OnPrimary = Color(0xFFFFFFFF)
+// Light Theme
+val md_theme_light_primary = Color(0xFF0079E9)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFF005BB5)
+val md_theme_light_onPrimaryContainer = Color(0xFFFFFFFF)
 
-val Tertiary = Color(0xFF006D36)
-val OnTertiary = Color(0xFFFFFFFF)
+val md_theme_light_secondary = Color(0xFF4CAF50)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFC8E6C9)
+val md_theme_light_onSecondaryContainer = Color(0xFF00210D)
 
-val RedButton = Color(0xFFBA1A1A)
-val OnRedButton = Color(0xFFFFFFFF)
+val md_theme_light_tertiary = Color(0xFF00796B)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFB2DFDB)
+val md_theme_light_onTertiaryContainer = Color(0xFF00201B)
 
-val Background = Color(0xFFF9F9FF)
-val OnBackground = Color(0xFF181C22)
+val md_theme_light_error = Color(0xFFD32F2F)
+val md_theme_light_errorContainer = Color(0xFFFFCDD2)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
 
-val Surface = Color(0xFFF1F3FC)
-val OnSurface = Color(0xFF181C22)
+val md_theme_light_background = Color(0xFFFFFFFF)
+val md_theme_light_onBackground = Color(0xFF1C1B1F)
+
+val md_theme_light_surface = Color(0xFFFFFFFF)
+val md_theme_light_onSurface = Color(0xFF1C1B1F)
+
+val md_theme_light_surfaceVariant = Color(0xFFF1F3FC)
+val md_theme_light_onSurfaceVariant = Color(0xFF44474F)
+
+val md_theme_light_outline = Color(0xFF717784)
+val md_theme_light_inverseOnSurface = Color(0xFFE6E1E5)
+val md_theme_light_inverseSurface = Color(0xFF313033)
+val md_theme_light_inversePrimary = Color(0xFF90C2FF)
+val md_theme_light_surfaceTint = Color(0xFF0079E9)
+val md_theme_light_outlineVariant = Color(0xFFCAC4D0)
+val md_theme_light_scrim = Color(0xFF000000)
+
+// Dark Theme
+val md_theme_dark_primary = Color(0xFF90C2FF)
+val md_theme_dark_onPrimary = Color(0xFF00315C)
+val md_theme_dark_primaryContainer = Color(0xFF004A87)
+val md_theme_dark_onPrimaryContainer = Color(0xFFD3E3FF)
+
+val md_theme_dark_secondary = Color(0xFF81C784)
+val md_theme_dark_onSecondary = Color(0xFF00391D)
+val md_theme_dark_secondaryContainer = Color(0xFF005230)
+val md_theme_dark_onSecondaryContainer = Color(0xFFA2F5C8)
+
+val md_theme_dark_tertiary = Color(0xFF80CBC4)
+val md_theme_dark_onTertiary = Color(0xFF003730)
+val md_theme_dark_tertiaryContainer = Color(0xFF005047)
+val md_theme_dark_onTertiaryContainer = Color(0xFFA0F0E0)
+
+val md_theme_dark_error = Color(0xFFFF6659)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690000)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val md_theme_dark_background = Color(0xFF121212)
+val md_theme_dark_onBackground = Color(0xFFE6E1E5)
+
+val md_theme_dark_surface = Color(0xFF121212)
+val md_theme_dark_onSurface = Color(0xFFE6E1E5)
+
+val md_theme_dark_surfaceVariant = Color(0xFF44474F)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCAC4D0)
+
+val md_theme_dark_outline = Color(0xFF8E8E93)
+val md_theme_dark_inverseOnSurface = Color(0xFF1C1B1F)
+val md_theme_dark_inverseSurface = Color(0xFFE6E1E5)
+val md_theme_dark_inversePrimary = Color(0xFF0079E9)
+val md_theme_dark_surfaceTint = Color(0xFF90C2FF)
+val md_theme_dark_outlineVariant = Color(0xFF4D4D4D)
+val md_theme_dark_scrim = Color(0xFF000000)
